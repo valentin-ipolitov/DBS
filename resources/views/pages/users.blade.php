a34ea311568c2b53fcc22e585c86431f2f6cc6dc
Valentyn Ipolitov <sv.vartey@gmail.com>
2020-01-11 05:15:42 +0100

    Added responsive design,

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -3,22 +3,26 @@
 
 @section('content')
     <div class="users">
-        <h1>Users table: </h1> 
+        <h1>Users table:</h1>
         <table>
-            <tr>
-                <th> Name </th>
-                <th> E-mail </th>
-                <th> Birth date </th>
-                <th> Gender </th>
-            </tr>
-            @foreach ($users as $user)
+            <thead>
                 <tr>
-                    <td> {{ $user->name }} </td>
-                    <td> {{ $user->email }} </td>
-                    <td> {{ $user->birtDate }} </td>
-                    <td> {{ $user->gender }} </td>
+                    <th> Name </th>
+                    <th> E-mail </th>
+                    <th> Birth date </th>
+                    <th> Gender </th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                <tr>
+                    <td data-label="name"> {{ $user->name }} </td>
+                    <td data-label="email"> {{ $user->email }} </td>
+                    <td data-label="birth_date"> {{ $user->birthDate }} </td>
+                    <td data-label="gender"> {{ $user->gender }} </td>
+                </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
 @endsection
